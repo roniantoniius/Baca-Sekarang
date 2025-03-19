6a3c22f8959cc3cdcbe086f5a229ca740c5afbd2
roniantonius <roniantoniuss@upi.edu>
2025-03-19 21:33:16 +0700

    Started and setup project configuration to build Baca Sekarang web application, which is a blog app.

---
+++ barang/src/main/java/com/roniantonius/barang/BarangApplication.java
@@ -0,0 +1,13 @@
+package com.roniantonius.barang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarangApplication.class, args);
+	}
+
+}
